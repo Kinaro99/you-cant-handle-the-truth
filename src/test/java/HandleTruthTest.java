96cd49fe632510fda25cbe2e43fc5d8a2f191aca
Kinaro99 <abbie687@uw.edu>
2020-11-21 21:38:11 -0800

    1.notes

--- src/test/java/HandleTruthTest.java
+++ src/test/java/HandleTruthTest.java
@@ -6,3 +6,11 @@ public class HandleTruthTest {
         HandleTruth.wordCount();
     }
 }
+
+//lecture notes for quiz
+//step 1: hashmap<string,integer> -> counts how many times each word appears
+//step 2: treemap<integer,string> -> sort in descending order
+    // prob: you 10x,cannot have 2 keys with same value
+    //increase string and add another word to string
+    //
+//step 3: do assertions -> test inputs/outputs & expected
